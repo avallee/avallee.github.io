5a3db51c25d84d1ca0dd2b369a0d4f2956807316
Alex Vallée <avallee@google.com>
2016-03-31 13:36:36 -0400

    Add window.screen test page.

---
+++ screen-test.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function(){
+  var s = window.screen;
+  document.getElementById('width').innerText = s.width;
+  document.getElementById('height').innerText = s.height;
+});
