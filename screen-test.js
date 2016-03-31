901d1a73fcb62d4b91651894435990734c3bc138
Alex Vallée <avallee@google.com>
2016-03-31 13:50:34 -0400

    Add pass/fail to screen-test.

--- screen-test.js
+++ screen-test.js
@@ -1,5 +1,22 @@
+var result = function(pass, opt_parent) {
+  var node = document.createElement('p');
+  node.style.fontWeight = 'bold';
+
+  node.innerText = pass ? 'PASSED' : 'FAIL';
+  node.style.color = pass ? 'green' : 'red';
+
+  if (opt_parent) {
+    opt_parent.appendChild(node);
+  }
+
+  return node;
+};
+
+
 document.addEventListener('DOMContentLoaded', function(){
   var s = window.screen;
   document.getElementById('width').innerText = s.width;
   document.getElementById('height').innerText = s.height;
+
+  result((s.width && s.height), document.body);
 });
